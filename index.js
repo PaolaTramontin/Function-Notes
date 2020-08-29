39137dabffb446f9078f3619556ece04ee3d7924
Paola Tramontin <paolatramontin1221@gmail.com>
2020-08-29 08:19:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+// sayHello()
+
+// // function declaration
+
+// function sayHello () {
+// //   //here's where the DOING IS DONE
+//    console.log("Hello, world!")
+// }
+
+// //function expression
+
+// // const sayHello = function() {
+// //   console.log("Hello, World")
+// // }
+
+// //invoke aka run the function 
+// //main difference between functiin declaration and expression is that declartion will read the code written before. function expression will not run the code before.
+// sayHello()
+
+// //ARROW function are also not hoisted 
+
+//const printBoo = () => {
+//   console.log('======')
+//   console.log('boo!')
+//   console.log('===')
+// }
+// //invoke an arrow function
+
+// printBoo()
+
+//let x = 10
+// const printSum = {
+   
+// }
+
+const printSum = () => {
+ console.log(10 + 10)
+}
+printSum();
+
+
+
+// #
+// ##
+// ###
+// ####
+
+// function printTriangle () {
+
+// }
+
+// const printTriangle = function () {
+//   console.log('#')
+//   console.log('##')
+//   console.log('###')
+//   console.log('####')
+//   console.log('#####')
+// }
+
+//printTriangle()
+
+
+//const printTriangle = function() {
+// for (let i=1; i<6; i++) {
+//   console.log('#'.repeat(i))
+// }
+// }
+// printTriangle()
+
+// console.log(printTriangle)
+
+
+
+// const sayName = (name) => {
+//   console.log('hello, my name is', name)
+// }
+
+// sayName('taylor')
+// sayName('Tom')
+// sayName('Timmy')
+
+
+// //const myFunc = (PARAMETER) => {
+//   //some code
+// //}
+
+// //myFunc(ARGUMENT)
+
+// const calcArea = (num1, num2) => {
+//   console.log(num1*num2)
+// }
+// calcArea(2,2)
+
+
+
+// function minusOne (num) {
+//   console.log(num-1)
+// }
+// minusOne(-100)
+
+// const makeSentence = (word1, word2, word3) => {
+//   console.log(word1, word2, word3)
+// }
+// makeSentence('i', 'want,'tacos)
+
+//const multiply = (num1, num2, num3) => {
+//   console.log(num1*num2*num3)
+// }
+// multiply(10, 5, 2)
+
+
+// const ten = () => {
+//   return 100
+// }
+// console.log(ten())
+
+
